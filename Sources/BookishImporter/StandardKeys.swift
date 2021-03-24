c3287f0705f3efe5f9c436e24edb3e6f99e4f3e0
Sam Deane <sam@elegantchaos.com>
2021-03-24 17:01:57 +0000

    Added standard keys

---
+++ Sources/BookishImporter/StandardKeys.swift
@@ -0,0 +1,26 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 24/03/21.
+//  All code (c) 2021 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import Foundation
+
+public extension String {
+    static let addedDateKey = "added"
+    static let asinKey = "asin"
+    static let authorsKey = "authors"
+    static let deweyKey = "dewey"
+    static let formatKey = "format"
+    static let genresKey = "genres"
+    static let heightKey = "height"
+    static let illustratorsKey = "illustrators"
+    static let isbnKey = "isbn"
+    static let lengthKey = "length"
+    static let modifiedDateKey = "modified"
+    static let pagesKey = "pages"
+    static let publishedDateKey = "published"
+    static let publishersKey = "publishers"
+    static let seriesKey = "series"
+    static let subtitleKey = "subtitle"
+    static let widthKey = "width"
+}
